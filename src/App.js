59849697647bbb9a77771616632305f65c9c3402
AmanjotG <74638021+AmanjotG@users.noreply.github.com>
2023-01-08 18:06:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from 'three'
+import { OrbitControls } from './OrbitControls';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // scene
+  const scene = new THREE.Scene();
+
+  // camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
   );
+  camera.position.z = 5;
+
+  const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256);
+
+  const cubeCamera = new THREE.CubeCamera(1, 1000, cubeRenderTarget);
+
+  //renderer
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //box
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00333366 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  // controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', animate);
+  controls.minDistance = 5;
+  controls.maxDistance = 50;
+  controls.enablePan = false;
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+
+    cubeCamera.update(renderer, scene)
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
+
 }
 
 export default App;
