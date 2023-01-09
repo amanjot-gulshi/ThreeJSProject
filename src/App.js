5878f2da2d68cb50f8be6ea3c5ed41ded5fbfc0f
AmanjotG <74638021+AmanjotG@users.noreply.github.com>
2023-01-09 13:51:55 -0500

    Adding table top with grid below cube

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import * as THREE from 'three'
 import { OrbitControls } from './OrbitControls';
 
 function App() {
+  //colors
+  const RED = 0x00990000
+  const WHITE = 0xffffff
+  const BLUE = 0x00333366
+
+
   // scene
   const scene = new THREE.Scene();
 
@@ -26,11 +32,30 @@ function App() {
   //box
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const edges = new THREE.EdgesGeometry(geometry);
-  const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
-  const material = new THREE.MeshBasicMaterial({ color: 0x00333366 });
+  const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: WHITE }));
+  const material = new THREE.MeshBasicMaterial({ color: BLUE });
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
-  scene.add(line)
+  scene.add(line);
+
+  //table
+  const tableGeometry = new THREE.BoxGeometry(10, 0.1, 10,);
+  const tableEdges = new THREE.EdgesGeometry(tableGeometry);
+  const tableLine = new THREE.LineSegments(tableEdges, new THREE.LineBasicMaterial({ color: WHITE }));
+  const tableMaterial = new THREE.MeshBasicMaterial({ color: RED });
+  const table = new THREE.Mesh(tableGeometry, tableMaterial);
+  table.position.y = -2;
+  tableLine.position.y = -2;
+  scene.add(table);
+  scene.add(tableLine);
+
+  //grid
+  const size = 10;
+  const divisions = 10;
+
+  const gridHelper = new THREE.GridHelper(size, divisions);
+  gridHelper.position.y = -1.95;
+  scene.add(gridHelper);
 
   // controls
   const controls = new OrbitControls(camera, renderer.domElement);
@@ -41,9 +66,6 @@ function App() {
 
   function animate() {
     requestAnimationFrame(animate);
-
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
 
     cubeCamera.update(renderer, scene)
 
